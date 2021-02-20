5b080e88aa634959627991091ab209f697ad5ac7
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-02-21 02:55:48 +0900

    팩토리얼 마지막 수
    주석 추가

--- 백준(Python)/Problem2553.py
+++ 백준(Python)/Problem2553.py
@@ -4,6 +4,6 @@ for i in range(1, N + 1):
     Num = Num * i
     while Num % 10 == 0:
         Num = Num // 10
-    Num = Num % 100000
+    Num = Num % 1000000  # 0이 아닌 뒷부분을 어느정도 챙기느냐에 따라 예측할 수 있는 N!의 범위가 늘어남
 
 print(Num % 10)
