e33eac1a5ba998083827dc79ba3e9affa7fd08e8
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-02-20 21:50:34 +0900

    Problem15654-N과M(5) -clear-
    Algorithm : Qsort, BackTracking

--- 백준/백준/13_백트래킹.c
+++ 백준/백준/13_백트래킹.c
@@ -1,6 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int Problem15654_backtracking(int depth, int width, int pos, int arr[], int Used_Check[], int Number_Flag[])
+{
+	int i;
+
+	/* 출력 파트 */
+	if (pos == width)
+	{
+		for (i = 0; i < width; i++)
+			printf("%d ", arr[i]);
+		printf("\n");
+	}
+	/* 재귀 파트 */
+	else
+	{
+		for (i = 0; i < depth; i++)
+			if (Used_Check[i] == 0)
+			{
+				arr[pos] = Number_Flag[i];
+				Used_Check[i] = 1;
+				Problem15654_backtracking(depth, width, pos + 1, arr, Used_Check, Number_Flag);
+				Used_Check[i] = 0;
+			}
+	}
+}
+
+int Problem15654_sort(int start, int end, int arr[])
+{
+	int pivot = arr[(start + end) / 2];
+	int low = start, high = end;
+	int temp;
+
+	while (low <= high)
+	{
+		while (arr[low] < pivot) low++;
+		while (pivot < arr[high]) high--;
+		if (low <= high)
+		{
+			temp = arr[low];
+			arr[low] = arr[high];
+			arr[high] = temp;
+			low++;
+			high--;
+		}
+	}
+	if (start < low - 1)
+		Problem15654_sort(start, low - 1, arr);
+	if (low < end)
+		Problem15654_sort(low, end, arr);
+}
+
+int Problem15654()
+{
+	int input_N, input_M, pos = 0;
+	int i;
+	int arr[8] = { 0 }, Used_Check[8] = { 0 }, Number_Flag[8] = { 0 };
+	scanf("%d%d", &input_N, &input_M);
+
+	for (i = 0; i < input_N; i++)
+		scanf("%d", &Number_Flag[i]);
+
+	/* 정렬 */
+	Problem15654_sort(0, input_N - 1, Number_Flag);
+	/* 백트래킹 */
+	Problem15654_backtracking(input_N, input_M, pos, arr, Used_Check, Number_Flag);
+}
+
 void Problem4_Tracking(int depth, int width, int pos, int num, int arr[])
 {
 	int i;
--- 백준/백준/main2.c
+++ 백준/백준/main2.c
@@ -2,8 +2,9 @@
 
 int main()
 {
-	backtracking_Problem4(); // 백트래킹 기본 + 중복 허용 + 비내림차순
-	//backtracking_Problem3(); // 백트래킹 기본 + 중복 허용
-	//backtracking_Problem2(); // 백트래킹 기본 + 오름차순
-	//backtracking_Problem1(); // 백트래킹 기본
+	Problem15654(); // N과 M(5) 백트래킹 기본 + 정렬 
+	//backtracking_Problem4(); // 15652번 백트래킹 기본 + 중복 허용 + 비내림차순
+	//backtracking_Problem3(); // 15651번 백트래킹 기본 + 중복 허용
+	//backtracking_Problem2(); // 15650번 백트래킹 기본 + 오름차순
+	//backtracking_Problem1(); // 15649번 백트래킹 기본
 }
