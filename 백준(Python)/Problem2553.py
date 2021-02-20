67a5cd2d871bba00af664c9038311f693cd49f10
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-02-21 02:06:42 +0900

    마지막 팩토리얼의 수 (오류 수정)

--- 백준(Python)/Problem2553.py
+++ 백준(Python)/Problem2553.py
@@ -2,10 +2,8 @@ N = int(input())
 Num = 1
 for i in range(1, N + 1):
     Num = Num * i
-    if Num % 1000000 == 0:
-        Num = Num // 1000000
-
-while Num % 10 == 0:
-    Num = Num // 10
+    while Num % 10 == 0:
+        Num = Num // 10
+    Num = Num % 100000
 
 print(Num % 10)
