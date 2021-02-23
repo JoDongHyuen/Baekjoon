7a24b719ea6b1682e520e7a88e70abedd2f002e1
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-02-23 15:18:58 +0900

    팰린드롬 수
    단순 구현 + 문자열 문제

--- 백준(Python)/Problem1259.py
+++ 백준(Python)/Problem1259.py
@@ -1,8 +1,25 @@
 
+# --------------------------------------
+# 팰린드롬 수
+# --------------------------------------
+
 while True:
+
+    flag = 0
+
     Arr = list(input())
     if Arr[0] == '0':
         break
-    print(type(Arr[0]))
-    Idx = len(Arr)
-    print(len(Arr))
+    length = len(Arr)
+    length -= 1
+    Idx = length // 2 + 1
+
+    for i in range(Idx):
+        if Arr[i] != Arr[length - i]:
+            flag = 1
+
+    if flag == 0:
+        print("yes")
+    else:
+        print("no")
+
