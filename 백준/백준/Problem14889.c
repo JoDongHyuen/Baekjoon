36f29dc16848d02c2e66fe00ed2981e15439ebae
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-02-28 23:12:29 +0900

    스타트와 링크 -시간 초과-
    반만 뽑으면 나머지 반은 정해진다는 점 + 오름차순 백트래킹 고려 못 해서 시간초과 남

---
+++ 백준/백준/Problem14889.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <math.h>
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+int STAT[20][20] = { 0 };
+int TEAM[20] = { 0 };
+int Used_Check[20] = { 0 };
+int MIN_GAP = 1000000;
+
+int Problem14889_Backtracking(int depth, int pos)
+{
+	int i, j;
+	int result, Team1_Sum, Team2_Sum;
+	
+	if (pos == depth)
+	{
+		Team1_Sum = 0, Team2_Sum = 0;
+		for (i = 0; i < (depth / 2); i++)
+			for (j = i; j < (depth / 2); j++) {
+				if (i != j) {
+					Team1_Sum = Team1_Sum + STAT[TEAM[i]][TEAM[j]] + STAT[TEAM[j]][TEAM[i]];
+					Team2_Sum = Team2_Sum + STAT[TEAM[i + (depth / 2)]][TEAM[j + (depth / 2)]] + STAT[TEAM[j + (depth / 2)]][TEAM[i + (depth / 2)]];
+				}
+			}
+		result = abs(Team1_Sum - Team2_Sum);
+		MIN_GAP = MIN(MIN_GAP, result);
+	}
+	else
+	{
+		for(i = 0; i < depth; i++)
+			if (Used_Check[i] == 0)
+			{
+				Used_Check[i] = 1;
+				TEAM[pos] = i;
+				Problem14889_Backtracking(depth, pos + 1);
+				Used_Check[i] = 0;
+			}
+	}
+}
+
+int Problem14889()
+{
+	int Human_Num;
+	int i, j;
+
+	/* 입력 파트 */
+	scanf("%d", &Human_Num);
+	for (i = 0; i < Human_Num; i++)
+		for (j = 0; j < Human_Num; j++)
+			scanf("%d", &STAT[i][j]);
+
+	/* 백트래킹 파트 */
+	Problem14889_Backtracking(Human_Num, 0);
+
+	/* 출력 파트 */
+	printf("%d\n", MIN_GAP);
+}
