f9188c935688b30ba236bb5e0935d75e4d19435f
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-03-03 14:56:14 +0900

    계단 오르기
    동적 계획법
    +) 새로운 풀이 추가

--- 백준(Python)/Problem2579.py
+++ 백준(Python)/Problem2579.py
@@ -3,7 +3,6 @@
 # --------------------------------------
 
 Step_Num = int(input())
-Step_Score = []
 Step_Score_Max = []
 
 
@@ -21,3 +20,27 @@ for i in range(Step_Num):
         Step_Score_Max[i].append(Score + Step_Score_Max[i - 1][0])
 
 print(max(Step_Score_Max[Step_Num - 1][0], Step_Score_Max[Step_Num - 1][1]))
+
+# ------------------------------------
+# 새로운 풀이
+# ------------------------------------
+Step_Num = int(input())
+Step_Score = []
+dp = []
+
+for i in range(Step_Num):
+    Score = int(input())
+    Step_Score.append(Score)
+    if i == 0:
+        dp.append(Score)
+    elif i == 1:
+        dp.append(dp[0] + Score)
+    elif i == 2:
+        dp.append(max(dp[0] + Score, Step_Score[1] + Score))
+    else:
+        # i번 째 계단을 밟았을 때
+        # 첫 번째 인자는 연속해서 밟은 계단이 1개 일 때
+        # 두 번째 인자는 연속해서 밟은 계단이 2개 일 때
+        dp.append(max(dp[i - 2] + Score, dp[i - 3] + Step_Score[i - 1] + Score))
+
+print(dp[Step_Num])
