0b481a1391c088bfdb562eed8426b4ecf3968f66
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-03-04 20:16:58 +0900

    1로 만들기
    동적 계획법
    (+ 2랑 3은 초기화 해줄 필요 없었음)

--- 백준(Python)/Problem1463.py
+++ 백준(Python)/Problem1463.py
@@ -14,9 +14,9 @@ for i in range(1, N + 1):
     elif i == 3:
         DP.append(1)
     else:
-        if i % 3 == 0 and i % 2 == 0:
-            DP.append(min(DP[i // 3], DP[i // 2]) + 1)
-        elif i % 3 == 0:
+        # if i % 3 == 0 and i % 2 == 0:
+        #     DP.append(min(DP[i // 3], DP[i // 2]) + 1)
+        if i % 3 == 0:
             DP.append(min(DP[i // 3], DP[i - 1]) + 1)
         elif i % 2 == 0:
             DP.append(min(DP[i // 2], DP[i -1]) + 1)
