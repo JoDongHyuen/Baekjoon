97b9c3563a298ed2788580caf5acd51541d4676f
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-03-04 20:07:06 +0900

    1로 만들기
    동적 계획법

---
+++ 백준(Python)/Problem1463.py
@@ -0,0 +1,26 @@
+# --------------------------------------
+# 1로 만들기
+# --------------------------------------
+
+
+N = int(input())
+DP = [0]
+
+for i in range(1, N + 1):
+    if i == 1:
+        DP.append(0)
+    elif i == 2:
+        DP.append(1)
+    elif i == 3:
+        DP.append(1)
+    else:
+        if i % 3 == 0 and i % 2 == 0:
+            DP.append(min(DP[i // 3], DP[i // 2]) + 1)
+        elif i % 3 == 0:
+            DP.append(min(DP[i // 3], DP[i - 1]) + 1)
+        elif i % 2 == 0:
+            DP.append(min(DP[i // 2], DP[i -1]) + 1)
+        else:
+            DP.append(DP[i - 1] + 1)
+
+print(DP[N])
