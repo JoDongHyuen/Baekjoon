1226aaab44a8e4565ca9db2ce71129f1fbf74735
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-03-04 20:33:36 +0900

    1로 만들기
    동적 계획법
    (+ 다른 풀이 방법 추가)

--- 백준(Python)/Problem1463.py
+++ 백준(Python)/Problem1463.py
@@ -9,18 +9,29 @@ DP = [0]
 for i in range(1, N + 1):
     if i == 1:
         DP.append(0)
-    elif i == 2:
-        DP.append(1)
-    elif i == 3:
-        DP.append(1)
     else:
-        # if i % 3 == 0 and i % 2 == 0:
-        #     DP.append(min(DP[i // 3], DP[i // 2]) + 1)
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 2 == 0:
+            DP.append(min(DP[i // 3], DP[i // 2]) + 1)
+        elif i % 3 == 0:
             DP.append(min(DP[i // 3], DP[i - 1]) + 1)
         elif i % 2 == 0:
             DP.append(min(DP[i // 2], DP[i -1]) + 1)
         else:
             DP.append(DP[i - 1] + 1)
+
+print(DP[N])
+
+
+# 다른 풀이 방법
+N = int(input())
+DP = [0, 0]
+
+for i in range(2, N + 1):
+    DP.append(DP[i - 1] + 1)
+
+    if i % 2 == 0:
+        DP[i] = min(DP[i], DP[i // 2] + 1)
+    if i % 3 == 0:
+        DP[i] = min(DP[i], DP[i // 3] + 1)
 
 print(DP[N])
