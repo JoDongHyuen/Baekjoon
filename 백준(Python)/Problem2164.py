2eea3cb3d666d73e4ab5f5cef402677ea7d5b579
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-03-04 21:42:44 +0900

    카드2
    큐 기본

---
+++ 백준(Python)/Problem2164.py
@@ -0,0 +1,27 @@
+# --------------------------------------
+# 카드2
+# --------------------------------------
+# 시간 초과 코드
+# N = int(input())
+
+# Card = list(range(1, N +1))
+
+# while len(Card) != 1:
+#     Card.pop(0)
+#     Card.append(Card[0])
+#     Card.pop(0)
+
+# print(Card[0])
+
+# ------------------------------------------- #
+# collection 모듈 활용
+from collections import deque
+N = int(input())
+
+Card = deque(range(1, N +1))
+
+while len(Card) != 1:
+    Card.popleft()
+    Card.append(Card[0])
+    Card.popleft()
+print(Card[0])
