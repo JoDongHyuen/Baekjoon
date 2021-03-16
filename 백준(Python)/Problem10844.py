c76835f1c996b778dc54b6c1ec0a82eb5479f3cd
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-03-16 18:11:39 +0900

    쉬운 계단 수
    동적 계획법
    (피보나치로 착각함..)

--- 백준(Python)/Problem10844.py
+++ 백준(Python)/Problem10844.py
@@ -2,14 +2,37 @@
 # 쉬운 계단 수
 # --------------------------------------
 
+# 테이블 완전히 잘못 세움;;
+# N = int(input())
+# DP = [9]
+# fibo0 = [0, 1]
+# fibo9 = [1, 1]
+
+# for i in range(1, N):
+#     if i > 1:
+#         fibo9.append((fibo9[i - 1] + fibo9[i - 2]) % 1000000000)
+#         fibo0.append((fibo0[i - 1] + fibo0[i - 2]) % 1000000000)
+#     DP.append((DP[i - 1] * 2 - fibo9[i - 1] - fibo0[i - 1]) % 1000000000)
+
+
+# print(DP[N - 1])
+
 N = int(input())
-DP = [9]
-fibo =[1, 2]
+DP = [[0 for col in range(100)] for row in range(10)]
+
+for i in range(1, 10):
+    DP[i][0] = 1
 
 for i in range(1, N):
-    DP.append((DP[i - 1] * 2 - fibo[i - 1]) % 1000000000)
-    if i > 1:
-        fibo.append(fibo[i - 2] + fibo[i - 1])
+    for j in range(10):
+        if j == 0:
+            DP[j][i] = DP[1][i - 1]
+        elif j == 9:
+            DP[j][i] = DP[8][i - 1]
+        else:
+            DP[j][i] = (DP[j - 1][i - 1] + DP[j + 1][i - 1]) % 1000000000
 
-
-print(DP[N - 1])
+dp_sum = 0
+for i in range(10):
+    dp_sum += DP[i][N - 1] % 1000000000
+print(dp_sum % 1000000000)
