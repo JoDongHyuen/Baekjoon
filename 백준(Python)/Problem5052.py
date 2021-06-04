a698b17be3d2baa7d5fea66929131b25e5b2830c
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-06-05 01:03:50 +0900

    [BOJ] 5052번 : 전화번호 목록
    Algorithm : 트라이

--- 백준(Python)/Problem5052.py
+++ 백준(Python)/Problem5052.py
@@ -2,14 +2,17 @@
 # 전화번호 목록
 # --------------------------------------
 
+import sys
+
 class Node(object):
-    def __init__(self, key):
+    def __init__(self, key, valid):
         self.key = key
+        self.valid = valid
         self.children = {}
 
 class Tire(object):
     def __init__(self):
-        self.head = Node(None)
+        self.head = Node(None, False)
 
     def Insert(self, string):
         curr_node = self.head
@@ -18,11 +21,17 @@ class Tire(object):
         for char in string:
             
             if not char in curr_node.children:
-                curr_node.children[char] = Node(char)
-                check = 1
+                curr_node.children[char] = Node(char, False)
+                if check == 0:
+                    check = 1
             
             curr_node = curr_node.children[char]
-        
+            
+            if curr_node.valid == True:
+                check = -1
+
+        curr_node.valid = True
+
         return check
 
 
@@ -31,16 +40,16 @@ Test_Case = int(input())
 for _ in range(Test_Case):
     Number_Num = int(input())
     List = Tire()
-    result = 0
+    result = True
 
     for i in range(Number_Num):
-        str = input()
+        #str = input()
+        str = sys.stdin.readline().strip()
         check = List.Insert(str)
-        if check == 0:
-            result = 1
+        if check != 1:
+            result = False
     
-    if result == 0:
+    if result == True:
         print("YES")
     else:
         print("NO")
-    
