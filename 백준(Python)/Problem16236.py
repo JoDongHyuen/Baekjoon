b23cb8f8a082ea0112de34bd272cdce489f58519
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-08-16 02:54:51 +0900

    [BOJ] 16236번 : 아기상어
    Algorithm : BFS
    
    BFS 알고리즘에서 queue에 넣을 때, vistied를 변경해주어야 하는데 그 부분을 안 했음..

--- 백준(Python)/Problem16236.py
+++ 백준(Python)/Problem16236.py
@@ -3,10 +3,11 @@
 # -------------------------------------- 
 
 from collections import deque
+import heapq
 import sys
 
-dx = [0, 1, -1, 0]
-dy = [1, 0, 0, -1]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 sea = []
 total_move = 0
@@ -19,14 +20,16 @@ def BFS(now_row, now_col, move):
     global eat_count, total_move, now_size
 
     visited = [[False for __ in range(N)] for _ in range(N)]
+    queue = []
+    eat = []
     queue = deque()
-    queue.append((now_row, now_col, move))
+    queue.append((move, now_row, now_col))
 
     while queue:
-        row, col, move_count = queue.popleft()
+        move_count, row, col = queue.popleft()
         visited[row][col] = True
 
-        for i in range(3,- 1, -1):
+        for i in range(4):
             nrow =  row + dy[i]
             ncol = col + dx[i]
             
@@ -34,47 +37,24 @@ def BFS(now_row, now_col, move):
             if ncol < 0 or ncol >= N: continue
             if visited[nrow][ncol] : continue
             if sea[nrow][ncol] == 0 or sea[nrow][ncol] == now_size:
-                queue.append((nrow, ncol, move_count + 1))
+                queue.append((move_count + 1, nrow, ncol))
                 continue
-            # 아기상어가 먹을 수 있는 먹이 발견
             if sea[nrow][ncol] < now_size:
-                move_count += 1
-                eat_count += 1
-                total_move += move_count
-                
-                # 아기상어가 우선 순위에 맞게 먹이를 찾아가게 하는 코드
-                while queue:
-                    chk_row, chk_col, move_chk = queue.popleft()
-                    visited[chk_row][chk_col] = True
+                heapq.heappush(eat, (move_count + 1, nrow, ncol))               
+    
+    if eat:
+        move, row, col = eat[0]
+        total_move += move
+        sea[row][col] = 0
 
-                    if move_chk < move_count:
-                        for j in range(3,- 1, -1):
-                            mrow = chk_row + dy[j]
-                            mcol = chk_col + dx[j]
-                            if mrow < 0 or mrow >= N: continue
-                            if mcol < 0 or mcol >= N: continue
-                            if visited[mrow][mcol] : continue
-                            if sea[mrow][mcol] == 0 : continue
-                            if sea[mrow][mcol] < now_size:
-                                queue.append((mrow, mcol, move_chk + 1))
+        eat_count += 1
+        if eat_count == now_size:
+            now_size += 1
+            eat_count = 0
+        
+        return row, col
 
-                    elif move_chk == move_count and sea[chk_row][chk_col] != 0 and sea[chk_row][chk_col] < now_size:
-                        if chk_row < nrow:
-                            nrow = chk_row
-                            ncol = chk_col
-                        elif chk_row == nrow and chk_col < ncol:
-                            nrow = chk_row
-                            ncol = chk_col                            
-                
-                # 아기상어 성장
-                if eat_count == now_size:
-                    now_size += 1
-                    eat_count = 0
-                
-                sea[nrow][ncol] = 0
-                return nrow, ncol
-
-    return -1, -1
+    else:   return -1, -1
 
 
 N = int(sys.stdin.readline())
