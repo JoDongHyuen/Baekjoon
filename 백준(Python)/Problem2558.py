b80dc0cd5a97dd78b8563c9b3819f50a0288a486
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-09-10 22:20:55 +0900

    [BOJ] 2558번 : A+B - 2
    Algorithm : 사칙연산

---
+++ 백준(Python)/Problem2558.py
@@ -0,0 +1,9 @@
+# --------------------------------------
+# A+B - 2
+# --------------------------------------
+
+import sys
+
+A = int(sys.stdin.readline())
+B = int(sys.stdin.readline())
+print(A+B)
