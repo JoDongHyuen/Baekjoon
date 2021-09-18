b7010d4e092802e4141485b728a2613ec26e8008
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2021-09-18 22:01:01 +0900

    [BOJ] 2338번 : 긴자리 계산
    Algorithm : 구현

---
+++ 백준(Python)/Problem2338.py
@@ -0,0 +1,12 @@
+# --------------------------------------
+# 긴자리 계산
+# --------------------------------------
+
+import sys
+
+A = int(sys.stdin.readline())
+B = int(sys.stdin.readline())
+
+print(A + B)
+print(A - B)
+print(A * B)
