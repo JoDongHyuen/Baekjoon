3c15c5152cd353b5215428a2dba05425f0f33f0e
JoDongHyuen <32729276+JoDongHyuen@users.noreply.github.com>
2022-01-03 16:18:55 +0900

    Update main2.c

--- 백준/백준/main2.c
+++ 백준/백준/main2.c
@@ -1,20 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+	/* 유니온 파인드 */
+	//Problem1043(); //거짓말
+	//Problem1717(); // 집합의 표현
+	Problem1976();
+
+	/* 연결 리스트 */
+	//Problem1021();
+
+	/* BFS */
+	//Problem1389();
+	//Problem1926();
+	//Problem2178();
+	//Problem1260();
+	//Problem1697();
+	//Problem4963();
+	//Problem2606(); // 바이러스
+
+	/* 힙 */
+	//Problem11279();
+	//Problem1927();
+
+	/* 트리 */
+	//Problem1991(); // 트리 순회
+	//Problem5639();
+	//Problem2263();
+
+	/* 동적 계획법 문제*/
+	//Problem5582(); // 공통 부분 문자열
+
+	/* 문자열 문제 */
+	//Problem1620(); // 포켓몬 마스터 이다솜
+	//Problem20112(); // 사토르 마방진
+	//Problem2902(); // KMP는 왜 KMP일까?
+
+	/* 재귀 문제 */
+	//Problem1074(); // Z
+	//Problem1780(); // 종이의 개수
+	//Problem1992(); //쿼드 트리
+
 	/* 실험실 코드 */
+	//test();
 	//malloc_test();
 	//static_alloc_test();
 	//pointer_test();
 	//char_pointer_test();
 	//pointer_and_array_test();
 	//funt_pointer();
+	//qutoe_test();
+	//str_change_test();
+	//doublepoint_test();
+	//strtok_test();
+	//tree_test();
+	//Friend_Test();
+	//printtest();
 
 	/* 브루트 포스 문제 */
-	Problem18111();
+	//Problem18111(); // 마인크래프트
 
 	/* 그리디 문제 */
 	//Problem1931(); // 회의실 배정
+	//Problem1541();
+	//Problem1339(); //단어 수학
 
 	/* 큐, 덱 문제 */
 	//Problem2164(); // 카드2
@@ -36,10 +86,13 @@ int main()
 	//Problem2493(); // 스택 활용 - 탑
 	//Problem1874(); // 스택 수열
 	//Problem10828(); // 스택 구현
+	//Problem17298(); // 오큰수
 
 	/* 백트래킹 문제 */
 	/* 백트래킹 유의 점 */
 	// 정렬이 필요한가? 중복된 결과를 걸러야 하는가?
+	//Problem1107(); // 리모컨
+	//Problem1012(); // 유기농 배추
 	//Problem1182(); // 부분수열의 합
 	//Problem14889(); // 스타트와 링크 문제
 	//Problem14888(); // 연산자 끼워넣기 문제
